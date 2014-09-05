be8542a6f02fbe517159ee3bbc150bfc78aa9f5c
Diego Camacho <dcamacho@americandiscountcruises.com>
2014-09-05 10:04:28 -0400

    This is my test fizzbuzz commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+for ($count = 1; $count <= 100; $count++) {
+    if ($count % 3 == 0 && $count % 5 == 0) {
+        print "FizzBuzz<br />";
+    } else if ($count % 3 == 0) {
+        print "Fizz<br />";
+    } else if ($count % 5 == 0) {
+        print "Buzz<br />";
+    } else {
+        print $count . "<br />";
+    }
+}
+
+?>
